77553d2ae9b4c12f12c5bdbc980407e4da114241
AMenezes97 <am83@princeton.edu>
2023-03-03 11:08:55 -0500

    Updated test-antibody_models.R with an additional test

--- tests/testthat/test-antibody_models.R
+++ tests/testthat/test-antibody_models.R
@@ -21,3 +21,16 @@ test_that("Check that antibody_model_biphasic function works", {
 })
 
 
+test_that("Check that antibody_model_biphasic function works", {
+ 
+   ## Load in example data and necessary arguments
+    model_pars <- read.csv("~/Documents/GitHub/serosim/inst/extdata/model_pars_typhoid.csv")
+    tmp_pars <- list()
+    tmp_pars[[1]] <- draw_parameters_random_fx(1,1,1,1,NULL,NULL,model_pars)
+    tmp_exposure_history <- array(0,dim=c(1,11,2))
+    tmp_exposure_history[1,1,1] <- 1
+    outcome<-antibody_model_typhoid(1,10, 1, tmp_exposure_history, NULL, tmp_pars,example_biomarker_map_numeric)
+
+  ## Expect that the output of antibody_model_biphasic function is 0.075000975
+  expect_equal(is.numeric(outcome),TRUE)
+})
