5a2d7ded80539313d6a13d55f5d577709a4e0997
James Hay <jameshay218@gmail.com>
2023-03-28 14:26:20 +0100

    Added repo for install packages in setup R file

--- R/install_packages.R
+++ R/install_packages.R
@@ -1,6 +1,6 @@
 list.of.packages <- c("ggplot2", "tidyverse","data.table","patchwork","reshape2", "tidyr","dplyr", "deSolve", "stats")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
 
 library(tidyverse)
 library(data.table)
